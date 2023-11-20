fe18a8d086ac6276454834227e06481701aad0ad
Manuel Del Giudice <manudg02@gmail.com>
2023-11-20 16:57:59 +0100

    20.11.23

--- 1Progetto Build Week/assets/js/index.js
+++ 1Progetto Build Week/assets/js/index.js
@@ -1,7 +1,7 @@
-const form = document.querySelector('form');
-form.addEventListener('submit', openDomande);
+const form = document.querySelector("form");
+form.addEventListener("submit", openDomande);
 
 function openDomande(event) {
-    event.preventDefault();
-    window.open('domande.html', '_self');
+  event.preventDefault();
+  window.open("domande.html", "_self");
 }
